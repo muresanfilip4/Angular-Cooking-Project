3b46663d9c716286670dff7eaf691e6af2803ef4
MURESAN FILIP <muresanfilip4@gmail.com>
2023-12-04 16:22:23 +0200

    observables added

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent  implements OnInit, OnDestroy{
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private slService: ShoppingListService) {
 
@@ -16,8 +18,12 @@ export class ShoppingListComponent {
   
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+    this.subscription = this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
       this.ingredients = ingredients;
     });
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe();
   }
 }
