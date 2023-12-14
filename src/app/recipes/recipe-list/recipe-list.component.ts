b1c2a8870eb984f1e07d5034f9316f7d276cc49a
MURESAN FILIP <muresanfilip4@gmail.com>
2023-12-14 13:25:31 +0200

    forms added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
+import { Subscription } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Component} from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -9,8 +10,9 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit, OnDestroy{
   recipes: Recipe[];
+  subscription: Subscription;
 
   constructor(private recipeService: RecipeService, 
               private router: Router,
@@ -19,11 +21,18 @@ export class RecipeListComponent {
   }
 
   ngOnInit() {
+    this.subscription = this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
+      this.recipes = recipes;
+    });
     this.recipes = this.recipeService.getRecipes();
   }
   
   onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
